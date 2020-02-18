4ff078a9a803104994ae94db951c7cf8bce03549
josh <github@joshhighet.com>
2020-02-18 20:45:56 +1300

    Update argotun-interactive.sh

--- argotun-interactive.sh
+++ argotun-interactive.sh
@@ -10,17 +10,17 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 #https tunnel
-printf "primary FQDN [i.e bikinibottom.joshhighet.com] : "
+printf "primary FQDN [i.e bikinibottom.joshhighet.com] : " | lolcat --animate
 read hostname
-printf "local binding URL [i.e https://localhost:8000] : "
+printf "local binding URL [i.e https://localhost:8000] : " | lolcat --animate
 read url
-printf "tag [i.e bikinibottom=https] - enter for no tags : "
+printf "tag [i.e bikinibottom=https] - enter for no tags : " | lolcat --animate
 read tag
-logfile="/var/log/cloudflared-https.log"
+logfile="/var/log/cloudflared-https.log" | lolcat --animate
 #ssh tunnel
-printf "secondary FQDN for SSH [i.e ssh-bikinibottom.joshhighet.com] : "
+printf "secondary FQDN for SSH [i.e ssh-bikinibottom.joshhighet.com] : " | lolcat --animate
 read sshhostname
-printf "tag [i.e bikinibottom=ssh] - enter for no tags : "
+printf "tag [i.e bikinibottom=ssh] - enter for no tags : " | lolcat --animate
 read tag
 sshurl="ssh://localhost:22"
 sshlogfile="/var/log/cloudflared-ssh.log"
@@ -34,7 +34,7 @@ mkdir /etc/cloudflared
 wget --quiet https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb \
 -O /tmp/cloudflared.deb
 #printf "installing cloudflared\n\n"
-sudo dpkg -i /tmp/cloudflared.deb
+sudo dpkg -i /tmp/cloudflared.deb > /dev/null
 #printf "creating cloudflared config file\n\n"
 touch /etc/cloudflared/config.yml
 #printf "creating cloudflared logfile\n\n"
@@ -49,7 +49,9 @@ echo "pidfile: /etc/cloudflared/pid" >> /etc/cloudflared/config.yml
 #printf "checking for cloudflared updates\n\n"
 /usr/local/bin/cloudflared update
 #printf "authenticating argo tunnel\n\n"
+printf "\n"
 /usr/local/bin/cloudflared login
+printf "\n"
 #printf "enabling cloudflared as boot-start service\n\n"
 /usr/local/bin/cloudflared service install
 ######################################
@@ -62,7 +64,8 @@ mkdir /etc/cloudflared-ssh
 wget --quiet https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz \
 -O /etc/cloudflared-ssh/cloudflared.tgz
 #printf "unpacking cloudflared-ssh tarball\n\n"
-tar -xvf /etc/cloudflared-ssh/cloudflared.tgz -C /etc/cloudflared-ssh
+tar -xvf /etc/cloudflared-ssh/cloudflared.tgz -C /etc/cloudflared-ssh > /dev/null
+rm /etc/cloudflared-ssh/cloudflared.tgz
 #printf "creating cloudflared-ssh config file\n\n"
 touch /etc/cloudflared-ssh/config.yml
 #printf "creating cloudflared-ssh logfile\n\n"
@@ -77,7 +80,7 @@ echo "pidfile: /etc/cloudflared-ssh/pid" >> /etc/cloudflared-ssh/config.yml
 #printf "checking for cloudflared-ssh updates\n\n"
 /etc/cloudflared-ssh/cloudflared update
 #printf "configuring cloudflared-ssh systemd files\n\n"
-echo "[Unit]
+echo """[Unit]
 Description=Argo Tunnel
 After=network.target
 
@@ -89,28 +92,34 @@ Restart=on-failure
 RestartSec=5s
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/cloudflared-ssh.service
-echo "[Unit]
+WantedBy=multi-user.target""" > /etc/systemd/system/cloudflared-ssh.service
+echo """[Unit]
 Description=Update Argo Tunnel
 
 [Timer]
 OnUnitActiveSec=1d
 
 [Install]
-WantedBy=timers.target" > /etc/systemd/system/cloudflared-ssh-update.timer
-echo "[Unit]
+WantedBy=timers.target""" > /etc/systemd/system/cloudflared-ssh-update.timer
+echo """[Unit]
 Description=Update Argo Tunnel
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c '/etc/cloudflared-ssh/cloudflared update; code=$?; if [ $code -eq 64 ]; then systemctl restart cloudflared-ssh; exit 0; fi; exit $code" \
+ExecStart=/bin/bash -c '/etc/cloudflared-ssh/cloudflared update; code=$?; if [ $code -eq 64 ]; then systemctl restart cloudflared-ssh; exit 0; fi; exit $code""" \
+> /etc/systemd/system/cloudflared-ssh-update.service
+echo """[Unit]
+Description=Update Argo Tunnel
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c '/usr/local/bin/cloudflared update; code=$?; if [ $code -eq 64 ]; then systemctl restart cloudflared; exit 0; fi; exit $code'""" \
 > /etc/systemd/system/cloudflared-ssh-update.service
 chmod 644 /etc/systemd/system/cloudflared-ssh.service
 chmod 644 /etc/systemd/system/cloudflared-ssh-update.timer
 chmod 644 /etc/systemd/system/cloudflared-ssh-update.service
 systemctl enable cloudflared-ssh.service
 systemctl enable cloudflared-ssh-update.timer
-systemctl enable cloudflared-ssh-update.service
 systemctl start cloudflared-ssh.service
 systemctl start cloudflared-ssh-update.timer
 systemctl start cloudflared-ssh-update.service
